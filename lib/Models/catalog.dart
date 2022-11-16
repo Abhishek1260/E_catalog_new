4e8a45092b815305dca2bd9ef1fd19f0f6589c6e
Abhishek Bansal <abhishekbansal938@gmail.com>
2022-11-16 15:21:12 +0530

    day10

---
+++ lib/Models/catalog.dart
@@ -0,0 +1,16 @@
+class Products {
+  final int id;
+  final String name;
+  final String desc;
+  final int price;
+  final String color;
+  final String image;
+
+  Products(
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.price,
+      required this.color,
+      required this.image});
+}
