f518a366ee03370f6d2520bcb8216ad81d133f1f
Abhishek Bansal <abhishekbansal938@gmail.com>
2022-11-17 16:01:58 +0530

    day18

--- lib/screens/cart_details.dart
+++ lib/screens/cart_details.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_17/Themes/theme.dart';
 
@@ -11,6 +12,83 @@ class CartPage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         title: const Text("Cart"),
+      ),
+      body: Column(
+        children: const [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: CartWidget(),
+            ),
+          ),
+          Divider(),
+          CartTotal(),
+        ],
+      ),
+    );
+  }
+}
+
+class CartWidget extends StatefulWidget {
+  const CartWidget({super.key});
+
+  @override
+  State<CartWidget> createState() => _CartWidgetState();
+}
+
+class _CartWidgetState extends State<CartWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, builder) {
+        return const ListTile(
+          leading: Icon(Icons.done),
+          trailing: IconButton(
+            onPressed: null,
+            icon: Icon(CupertinoIcons.clear_thick),
+          ),
+          title: Text("Item 1"),
+        );
+      },
+    );
+  }
+}
+
+class CartTotal extends StatelessWidget {
+  const CartTotal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 32.0,
+        vertical: 0.0,
+      ),
+      child: SizedBox(
+        height: 75.0,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "\$9999",
+              style: TextStyle(
+                color: Colors.red[800],
+                fontSize: 32.0,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Buy"),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Theme.of(context).buttonColor,
+                shape: const StadiumBorder(),
+                fixedSize: const Size(100.0, 45.0),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
